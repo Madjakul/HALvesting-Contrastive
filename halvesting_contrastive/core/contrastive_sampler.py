2ec1e876ba8382128c3b485675831041eabafdcb
Madjakul <contact@madjakul.com>
2025-01-13 15:52:23 +0100

    feat: new file for efficient contrastive sampling

---
+++ halvesting_contrastive/core/contrastive_sampler.py
@@ -0,0 +1,19 @@
+# halvesting_contrastive/core/contra_sampler.py
+
+import logging
+import os
+
+import datasets
+import torch
+
+from halvesting_contrastive.core.formater import Formater
+
+
+class ContrastiveSampler:
+    """Class used to sample documents and passages for contrastive learning."""
+
+    def __init__(self):
+        pass
+
+    def __call__(self):
+        pass
